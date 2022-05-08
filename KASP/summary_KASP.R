92557620071a0b894aad90f14cc3f0b3d7c28035
auroramaurizio <auroramaurizio1@gmail.com>
2022-05-08 16:58:53 +0200

    summary KASP

--- KASP/summary_KASP.R
+++ KASP/summary_KASP.R
@@ -1,5 +1,6 @@
 suppressMessages(library(openxlsx))
-
+require(vcfR)
+setwd("/Users/maurizio.aurora/Documents/ip0/KASP")
 
 ############################
 #process the ratio dataframe
@@ -8,112 +9,81 @@ suppressMessages(library(openxlsx))
 list.files()
 ########
 
-ESOCA10_OCT = read.table("ESOCA10_OCT_markdup.cns", header = T)
-ESOCA17_OCT = read.table("ESOCA17_OCT_markdup.cns", header = T)
-ESOCA24T = read.table("ESOCA24T_markdup.cns", header = T)
-ESOCA29T = read.table("ESOCA29T_markdup.cns", header = T)
-ESOCA34T = read.table("ESOCA34T_markdup.cns", header = T)
-ESOCA40_T = read.table("ESOCA40_T_markdup.cns", header = T)
-ESOCA48_T = read.table("ESOCA48_T_markdup.cns", header = T)
-ESOCA54_T = read.table("ESOCA54_T_markdup.cns", header = T)
-ESOCA11_OCT = read.table("ESOCA11_OCT_markdup.cns", header = T)
-ESOCA18_OCT = read.table("ESOCA18_OCT_markdup.cns", header = T)
-ESOCA25T = read.table("ESOCA25T_markdup.cns", header = T)
-ESOCA30T = read.table("ESOCA30T_markdup.cns", header = T)
-ESOCA35_T = read.table("ESOCA35_T_markdup.cns", header = T)
-ESOCA41_T = read.table("ESOCA41_T_markdup.cns", header = T)
-ESOCA49_T = read.table("ESOCA49_T_markdup.cns", header = T)
-ESOCA55_T = read.table("ESOCA55_T_markdup.cns", header = T)
-ESOCA12_OCT = read.table("ESOCA12_OCT_markdup.cns", header = T)
-ESOCA15_OCT= read.table("ESOCA15_OCT_markdup.cns", header = T)
-ESOCA20T= read.table("ESOCA20T_markdup.cns", header = T)
-ESOCA15_OCT= read.table("ESOCA15_OCT_markdup.cns", header = T)
-ESOCA26T= read.table("ESOCA26T_markdup.cns", header = T)
-ESOCA31T= read.table("ESOCA31T_markdup.cns", header = T)
-ESOCA22T= read.table("ESOCA22T_markdup.cns", header = T)
-ESOCA37_T= read.table("ESOCA37_T_markdup.cns", header = T)
-ESOCA43_T= read.table("ESOCA43_T_markdup.cns", header = T)
-ESOCA51_T= read.table("ESOCA51_T_markdup.cns", header = T)
-ESOCA57_T= read.table("ESOCA57_T_markdup.cns", header = T)
-ESOCA27T= read.table("ESOCA27T_markdup.cns", header = T)
-ESOCA33T= read.table("ESOCA33T_markdup.cns", header = T)
-ESOCA39_T= read.table("ESOCA39_T_markdup.cns", header = T)
-ESOCA45_T= read.table("ESOCA45_T_markdup.cns", header = T)
-ESOCA52_T= read.table("ESOCA52_T_markdup.cns", header = T)
-ESOCA8_OCT= read.table("ESOCA8_OCT_markdup.cns", header = T)
+UPN02_DG = vcfR::read.vcfR("UPN02_DG_KASP.vcf.gz")
+UPN02_PG = vcfR::read.vcfR("UPN02_PG_KASP.vcf.gz")
+UPN02_PRE = vcfR::read.vcfR("UPN02_PRE_KASP.vcf.gz")
+UPN02_REL = vcfR::read.vcfR("UPN02_REL_KASP.vcf.gz")
+
+UPN03_DG = vcfR::read.vcfR("UPN03_DG_KASP.vcf.gz")
+UPN03_PG = vcfR::read.vcfR("UPN03_PG_KASP.vcf.gz")
+UPN03_PRE = vcfR::read.vcfR("UPN03_PRE_KASP.vcf.gz")
+UPN03_REL = vcfR::read.vcfR("UPN03_REL_KASP.vcf.gz")
+
+UPN04_DIA = vcfR::read.vcfR("UPN04_DIA_KASP.vcf.gz")
+UPN04_DG = vcfR::read.vcfR("UPN04_DG_KASP.vcf.gz")
+UPN04_PG = vcfR::read.vcfR("UPN04_PG_KASP.vcf.gz")
+UPN04_PRE = vcfR::read.vcfR("UPN04_PRE_KASP.vcf.gz")
+UPN04_REL = vcfR::read.vcfR("UPN04_REL_KASP.vcf.gz")
+
+UPN05_DG = vcfR::read.vcfR("UPN05_DG_KASP.vcf.gz")
+UPN05_PG = vcfR::read.vcfR("UPN05_PG_KASP.vcf.gz")
+UPN05_PRE = vcfR::read.vcfR("UPN05_PRE_KASP.vcf.gz")
+UPN05_REL = vcfR::read.vcfR("UPN05_REL_KASP.vcf.gz")
+
+UPN06_DG = vcfR::read.vcfR("UPN06_DG_KASP.vcf.gz")
+UPN06_PG = vcfR::read.vcfR("UPN06_PG_KASP.vcf.gz")
+UPN06_PRE = vcfR::read.vcfR("UPN06_PRE_KASP.vcf.gz")
+UPN06_REL = vcfR::read.vcfR("UPN06_REL_KASP.vcf.gz")
 
 
 dfList = list(
-  ESOCA10_OCT,
-  ESOCA17_OCT,
-  ESOCA24T,
-  ESOCA29T,
-  ESOCA34T, 
-  ESOCA40_T,
-  ESOCA48_T, 
-  ESOCA54_T,
-  ESOCA11_OCT,
-  ESOCA18_OCT,
-  ESOCA25T,
-  ESOCA30T,
-  ESOCA35_T,
-  ESOCA41_T, 
-  ESOCA49_T, 
-  ESOCA55_T,
-  ESOCA12_OCT,
-  ESOCA15_OCT,
-  ESOCA20T,
-  ESOCA15_OCT,
-  ESOCA26T,
-  ESOCA31T,
-  ESOCA22T,
-  ESOCA37_T,
-  ESOCA43_T,
-  ESOCA51_T,
-  ESOCA57_T,
-  ESOCA27T,
-  ESOCA33T,
-  ESOCA39_T,
-  ESOCA45_T,
-  ESOCA52_T,
-  ESOCA8_OCT)
-
+  UPN02_DG,
+  UPN02_PG,
+  UPN02_PRE,
+  UPN02_REL,
+  UPN03_DG,
+  UPN03_PG,
+  UPN03_PRE,
+  UPN03_REL,
+  UPN04_DIA,
+  UPN04_DG,
+  UPN04_PG,
+  UPN04_PRE,
+  UPN04_REL,
+  UPN05_DG,
+  UPN05_PG,
+  UPN05_PRE,
+  UPN05_REL,
+  UPN06_DG,
+  UPN06_PG,
+  UPN06_PRE,
+  UPN06_REL
+)
 
 
 names(dfList)<- list(
-  "ESOCA10",
-  "ESOCA17",
-  "ESOCA24",
-  "ESOCA29",
-  "ESOCA34", 
-  "ESOCA40",
-  "ESOCA48", 
-  "ESOCA54",
-  "ESOCA11",
-  "ESOCA18",
-  "ESOCA25",
-  "ESOCA30",
-  "ESOCA35",
-  "ESOCA41", 
-  "ESOCA49", 
-  "ESOCA55",
-  "ESOCA12",
-  "ESOCA15",
-  "ESOCA20",
-  "ESOCA15",
-  "ESOCA26",
-  "ESOCA31",
-  "ESOCA22",
-  "ESOCA37",
-  "ESOCA43",
-  "ESOCA51",
-  "ESOCA57",
-  "ESOCA27",
-  "ESOCA33",
-  "ESOCA39",
-  "ESOCA45",
-  "ESOCA52",
-  "ESOCA8")
+  "UPN02_DG",
+  "UPN02_PG",
+  "UPN02_PRE",
+  "UPN02_REL",
+  "UPN03_DG",
+  "UPN03_PG",
+  "UPN03_PRE",
+  "UPN03_REL",  
+  "UPN04_DIA",
+  "UPN04_DG",
+  "UPN04_PG",
+  "UPN04_PRE",
+  "UPN04_REL", 
+  "UPN05_DG",
+  "UPN05_PG",
+  "UPN05_PRE",
+  "UPN05_REL", 
+  "UPN06_DG",
+  "UPN06_PG",
+  "UPN06_PRE",
+  "UPN06_REL" 
+)
 
 length(dfList)
 
@@ -121,240 +91,58 @@ datalist = list()
 
 
 for (i in names(dfList)) {
-  df_ratio <- dfList[[i]]
-  # remove Antitarget from the table
-  df_ratio = df_ratio[!grepl("Antitarget", df_ratio$gene),]
-  
-  #print(head(df_ratio))
-  
+  vcf <- dfList[[i]]
+  df_temp = vcfR2tidy(vcf)
+  df = df_temp$fix
   # select columns of interest
-  dftest_ratio = df_ratio[,c("chromosome","start","end","log2")]
+  dfsub = df[,c("CHROM","POS","AF")]
   # add the length coord to dftest
-  dftest_ratio$length = dftest_ratio$end -  dftest_ratio$start
-  head(dftest_ratio)
-  # sum the total length
-  all = sum(dftest_ratio[, 'length'])
-  head(all)
-  # get only values higher than 0.5
-  gain = dftest_ratio[dftest_ratio$log2 > 0.5, ]
-  tot_gain = sum(gain[, 'length'])
-  # get only values lower than -0.5
-  loss = dftest_ratio[dftest_ratio$log2 < -0.5, ]
-  tot_loss = sum(loss[, 'length'])
-  #sum gain loss
-  sum = tot_gain + tot_loss
-  # fraction 
-  GII = sum/all
-  
-  dat <- data.frame(i, GII)
-  colnames(dat)=c("Patient","GII")
+  dfsub$SAMPLE = i
+  dfsub$LOC = paste(dfsub$CHROM, dfsub$POS, sep="_")
+  dfn = dfsub[,c("LOC","AF","SAMPLE")]
+  dat <- data.frame(dfn)
+  colnames(dat)=c("LOC","AF","SAMPLE")
   datalist[[i]] <- dat
 }
 
-big_data = do.call(rbind, datalist) 
-colnames(big_data)=c("Patient","GII")
 
 
-WESsamplesESOCAupdate20220117 = read.table(file= "file.tsv", header = T)
-
-head(WESsamplesESOCAupdate20220117)
-#WESsamplesESOCAupdate20220117 = read.xlsx(file= "WESsamplesESOCAupdate20220117.xlsx", sheet = 2)
-
-head(WESsamplesESOCAupdate20220117)
-
-library(dplyr)
-library(stringr)
-mandard <- c("1", "2","4","5")
-responders <- c("1", "2")
-non_responders <- c("4","5")
-
-
-mySubset <- WESsamplesESOCAupdate20220117[WESsamplesESOCAupdate20220117$Mandard %in% mandard, ]
-head(mySubset)
-
-mySubset$responders <- ifelse(mySubset$Mandard %in% responders,"responders","non-responders")
-
-head(mySubset)
-
-merged <- merge(big_data,mySubset,by="Patient")
-
-head(merged)
-
-
-merged$patients <- factor(merged$responders, 
-                          levels=c("responders", "non-responders"))
-
-# Basic violin plot
-p <- ggplot(merged, aes(x=patients, y=GII, fill=patients)) + 
-  geom_violin() +  geom_boxplot(width=0.1, fill="white") + scale_fill_manual(values=c("blue", "red"))
-
-pdf("GII_vln_abs05_cns.pdf")
-p + theme(strip.text = element_text(size=25)) + theme(text = element_text( size = 20)) + theme(legend.position="none")
-dev.off()
-
-
-
-library(ggplot2)
-# Basic violin plot
-p <- ggplot(merged, aes(x=responders, y=GII, fill=responders)) + 
-  geom_violin() +  geom_boxplot(width=0.1, fill="white")
-
-pdf("GII_vln_abs05_cns.pdf")
-p + theme(strip.text = element_text(size=25)) + theme(text = element_text( size = 20))
-dev.off()
-
-
-
-
-
-
-
-#############
-# only gain
-############
-
-
-for (i in names(dfList)) {
-  df_ratio <- dfList[[i]]
-  # remove Antitarget from the table
-  df_ratio = df_ratio[!grepl("Antitarget", df_ratio$gene),]
-  
-  #print(head(df_ratio))
-  
-  # select columns of interest
-  dftest_ratio = df_ratio[,c("chromosome","start","end","log2")]
-  # add the length coord to dftest
-  dftest_ratio$length = dftest_ratio$end -  dftest_ratio$start
-  head(dftest_ratio)
-  # sum the total length
-  all = sum(dftest_ratio[, 'length'])
-  head(all)
-  # get only values higher than 0.5
-  gain = dftest_ratio[dftest_ratio$log2 > 0.5, ]
-  tot_gain = sum(gain[, 'length'])
-  
-  # fraction 
-  GII = tot_gain/all
-  
-  dat <- data.frame(i, GII)
-  colnames(dat)=c("Patient","GII")
-  datalist[[i]] <- dat
-}
 
 big_data = do.call(rbind, datalist) 
-colnames(big_data)=c("Patient","GII")
+tail(big_data)
+colnames(big_data)=c("LOC","AF","SAMPLE")
 
+grouped_by_gene  <- big_data %>%
+  group_by(LOC) %>%
+  summarise(temp = toString(SAMPLE)) %>%
+  ungroup()
 
-WESsamplesESOCAupdate20220117 = read.table(file= "file.tsv", header = T)
+head(grouped_by_gene)
 
-head(WESsamplesESOCAupdate20220117)
-#WESsamplesESOCAupdate20220117 = read.xlsx(file= "WESsamplesESOCAupdate20220117.xlsx", sheet = 2)
+colnames(grouped_by_gene) = c("LOC", "sample_SNP")
 
-head(WESsamplesESOCAupdate20220117)
+grouped_by_gene$number_of_samples = count.fields(textConnection(grouped_by_gene$sample_SNP), sep = ",")
+
+head(grouped_by_gene)
+
+write.xlsx(grouped_by_gene,file= "summary_KASP.xlsx")
+
 
 library(dplyr)
-library(stringr)
-mandard <- c("1", "2","4","5")
-responders <- c("1", "2")
-non_responders <- c("4","5")
+library(tidyr)
+
+library(reshape2)
 
 
-mySubset <- WESsamplesESOCAupdate20220117[WESsamplesESOCAupdate20220117$Mandard %in% mandard, ]
-head(mySubset)
+mat <- acast(big_data, LOC ~ SAMPLE, function(x) {sort(as.character(x))[1]},
+      value.var = 'AF', fill = '0')
 
-mySubset$responders <- ifelse(mySubset$Mandard %in% responders,"responders","non-responders")
+class(mat) <- "numeric"
 
-head(mySubset)
+pheatmap(mat, show_rownames = F, cluster_rows = F, filename = 'Heatmap_KASP_clusterCols.pdf', cluster_cols = T)
 
-merged <- merge(big_data,mySubset,by="Patient")
+pheatmap(mat, show_rownames = F, cluster_rows = T, filename = 'Heatmap_KASP_clusterRows.pdf')
 
-head(merged)
-
-library(ggplot2)
-# Basic violin plot
-p <- ggplot(merged, aes(x=responders, y=GII, fill=responders)) + 
-  geom_violin() +  geom_boxplot(width=0.1, fill="white")
-
-pdf("GII_vln_abs05_gain_cns.pdf")
-p + theme(strip.text = element_text(size=25)) + theme(text = element_text( size = 20))
-dev.off()
-
-
-#############
-# only loss
-############
-
-
-for (i in names(dfList)) {
-  df_ratio <- dfList[[i]]
-  # remove Antitarget from the table
-  df_ratio = df_ratio[!grepl("Antitarget", df_ratio$gene),]
-  
-  #print(head(df_ratio))
-  
-  # select columns of interest
-  dftest_ratio = df_ratio[,c("chromosome","start","end","log2")]
-  # add the length coord to dftest
-  dftest_ratio$length = dftest_ratio$end -  dftest_ratio$start
-  head(dftest_ratio)
-  # sum the total length
-  all = sum(dftest_ratio[, 'length'])
-  head(all)
-  # get only values higher than 0.5
-  loss = dftest_ratio[dftest_ratio$log2 < 0.5, ]
-  tot_loss = sum(loss[, 'length'])
-  
-  # fraction 
-  GII = tot_loss/all
-  
-  dat <- data.frame(i, GII)
-  colnames(dat)=c("Patient","GII")
-  datalist[[i]] <- dat
-}
-
-big_data = do.call(rbind, datalist) 
-colnames(big_data)=c("Patient","GII")
-
-
-WESsamplesESOCAupdate20220117 = read.table(file= "file.tsv", header = T)
-
-head(WESsamplesESOCAupdate20220117)
-#WESsamplesESOCAupdate20220117 = read.xlsx(file= "WESsamplesESOCAupdate20220117.xlsx", sheet = 2)
-
-library(dplyr)
-library(stringr)
-mandard <- c("1", "2","4","5")
-responders <- c("1", "2")
-non_responders <- c("4","5")
-
-
-mySubset <- WESsamplesESOCAupdate20220117[WESsamplesESOCAupdate20220117$Mandard %in% mandard, ]
-head(mySubset)
-
-mySubset$responders <- ifelse(mySubset$Mandard %in% responders,"responders","non-responders")
-
-head(mySubset)
-
-merged <- merge(big_data,mySubset,by="Patient")
-
-head(merged)
-
-library(ggplot2)
-# Basic violin plot
-p <- ggplot(merged, aes(x=responders, y=GII, fill=responders)) + 
-  geom_violin() +  geom_boxplot(width=0.1, fill="white")
-
-pdf("GII_vln_abs05_loss_cns.pdf")
-p + theme(strip.text = element_text(size=25)) + theme(text = element_text( size = 20))
-dev.off()
-
-getwd()
-
-
-
-
-
-
-
+write.xlsx(mat,file= "mat_KASP.xlsx")
 
 
