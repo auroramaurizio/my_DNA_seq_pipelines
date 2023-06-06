a62ce0aa8a4a53e2d92b470bde27a9f7ef83a28b
auroramaurizio <auroramaurizio1@gmail.com>
2023-06-06 13:08:57 +0200

    ega sub

--- EGA_sub/submit_to_EGA.sh
+++ EGA_sub/submit_to_EGA.sh
@@ -1,3 +1,7 @@
+#https://networklessons.com/uncategorized/lftp-stuck-making-data-connection
+#https://lftp.uniyar.ac.narkive.com/z4CSn40B/net-max-retries-bug
+
+
 lftp -d -u ega-box-XXXX,PASSWORD ftp.ega.ebi.ac.uk
 set net:max-retries 1
 set [ftp:ssl-allow]ftp:ssl-allow off
