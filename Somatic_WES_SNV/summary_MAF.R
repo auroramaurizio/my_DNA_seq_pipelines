8e1df91132d42469ba7e4923739a3edb241f5fde
auroramaurizio <auroramaurizio1@gmail.com>
2023-08-14 10:10:42 +0200

    mod

--- Somatic_WES_SNV/summary_MAF.R
+++ Somatic_WES_SNV/summary_MAF.R
@@ -13,6 +13,49 @@
 library(maftools)
 
 
+library(maftools)
+
+
+setwd="/Users/maurizio.aurora/Downloads"
+getwd()
+
+ESOCA20 = read.maf('ESOCA20_somatic.filtered.COSMIC.funcotator.maf') 
+
+getSampleSummary(ESOCA20)
+getGeneSummary(ESOCA20)
+
+?getSampleSummary
+
+1 #version 2.4
+14 3'Flank
+  34 3'UTR
+19 5'Flank
+  18 5'UTR
+66 Frame_Shift_Del #
+3 Frame_Shift_Ins #
+16 IGR
+10 In_Frame_Del #
+447 Intron
+233 Missense_Mutation #
+11 Nonsense_Mutation #
+35 RNA
+100 Silent
+32 Splice_Region
+4 Splice_Site #
+1 Variant_Classification
+
+66+3+10+233+11+4
+#Shows gene summary.
+getGeneSummary(laml.maf)
+#shows clinical data associated with samples
+getClinicalData(laml.maf)
+#Shows all fields in MAF
+getFields(laml)
+#Writes maf summary to an output file with basename laml.
+write.mafSummary(maf = laml.maf, basename = 'laml')
+
+
+
 #MAF_DellabonaP_480_NeoAg_esophagusK
 
 ESOCA8 = read.maf('/Users/maurizio.aurora/Dropbox (HSR Global)/WORKSPACE/ESOCA_mutect/MAF_esophagusK/ESOCA8_somatic.filtered.COSMIC.funcotator.maf') 
